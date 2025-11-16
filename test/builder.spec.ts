9b37701b798dd07b47cac19bc52a508643932bea
Ethan Snow <bombitmanbomb@gmail.com>
2025-11-16 14:35:58 -0500

    add more testing

--- test/builder.spec.ts
+++ test/builder.spec.ts
@@ -8,6 +8,7 @@ import {
   SystemType,
 } from '../src/';
 const preset1 = readFileSync(__dirname + '/data/NewPreset_1.powerpreset');
+const preset2 = readFileSync(__dirname + '/data/NewPreset_2.powerpreset');
 
 describe('Build a Preset', () => {
   test('Match Values 1', () => {
@@ -47,8 +48,36 @@ describe('Build a Preset', () => {
 
     expect(generatedPreset.toFile()).toEqual(powerpreset);
   });
-  // test.skip('Match Values 2', () => {
-  //   const decoder = new TextDecoder();
-  //   const powerpreset = decoder.decode(preset2);
-  // });
+  test('Match Values 2', () => {
+    const decoder = new TextDecoder();
+    const powerpreset = decoder.decode(preset2);
+    const generatedPreset = new PowerProfileBuilder()
+      .setShip('RSI_Polaris')
+      .setData((data) =>
+        data
+          .setSystem([
+            (sys) =>
+              sys.setType(SystemType.THRUSTER).setPips(1).setOnline(true),
+            (sys) => sys.setType(SystemType.SHIELD).setPips(5).setOnline(true),
+            (sys) =>
+              sys.setType(SystemType.TRACTOR_BEAM).setPips(2).setOnline(true),
+            new System().setType(SystemType.WEAPON).setPips(1).setOnline(true),
+          ])
+          .setHardpoint([
+            (hp) => hp.setType(HardpointType.COOLER).setPips(3).setOnline(true),
+            (hp) =>
+              hp.setType(HardpointType.LIFE_SUPPORT).setPips(2).setOnline(true),
+            (hp) =>
+              hp
+                .setType(HardpointType.QUANTUM_DRIVE)
+                .setPips(8)
+                .setOnline(true),
+            new Hardpoint()
+              .setType(HardpointType.RADAR)
+              .setPips(5)
+              .setOnline(true),
+          ])
+      );
+    expect(generatedPreset.toFile()).toEqual(powerpreset);
+  });
 });
